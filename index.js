afaf1d6f321a6af886876f13040456edd6513388
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-27 16:41:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
 
-  return Object.delete({}, object, {key});
+  delete Object.assign({}, object, {key});
   return object;
 }
 
